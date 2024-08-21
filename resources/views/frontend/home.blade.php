82bc51af3eff141949c22a625d7e9dcbd04480ed
Dwicky Candra Lesmana <dwickycandralesmana@gmail.com>
2024-08-21 19:17:51 +0800

    fix-tampilan

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -15,20 +15,30 @@
             </div>
 
             <div class="col-12 text-center mt-3">
-                <h3>Cek Tagihan Siswa</h3>
+                <h3>
+                    Cek Tagihan Siswa<br>
+                    <small class="text-muted">Silahkan masukkan NIS siswa dan pilih jenjang untuk mengecek tagihan</small>
+
+                </h3>
                 <form action="#" method="GET">
-                    <div class="form-group mb-3">
-                        <label class="form-label fw-bold" for="jenjang_id">Jenjang</label>
-                        <select class="form-select" id="jenjang_id" name="jenjang_id" required>
-                            <option value="">Pilih Jenjang</option>
-                            @foreach ($jenjangs as $jenjang)
-                                <option value="{{ $jenjang->id }}" @selected($jenjang->id == $jenjangId)>{{ $jenjang->nama }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group mb-3">
-                        <label class="form-label fw-bold" for="nis">NIS</label>
-                        <input type="text" class="form-control" id="nis" name="nis" required placeholder="Masukkan NIS" value="{{ $nis }}">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+                            <div class="form-group mb-3">
+                                <label class="form-label fw-bold" for="jenjang_id">Jenjang</label>
+                                <select class="form-select" id="jenjang_id" name="jenjang_id" required>
+                                    <option value="">Pilih Jenjang</option>
+                                    @foreach ($jenjangs as $jenjang)
+                                        <option value="{{ $jenjang->id }}" @selected($jenjang->id == $jenjangId)>{{ $jenjang->nama }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-12">
+                            <div class="form-group mb-3">
+                                <label class="form-label fw-bold" for="nis">NIS</label>
+                                <input type="text" class="form-control" id="nis" name="nis" required placeholder="Masukkan NIS" value="{{ $nis }}">
+                            </div>
+                        </div>
                     </div>
                     <button type="submit" class="btn btn-primary">
                         <i class="fas fa-search me-1"></i>
@@ -57,15 +67,25 @@
                                 @endphp
 
                                 <tr>
-                                    <td>{{ $loop->iteration }}</td>
+                                    @if($loop->last)
+                                        <td></td>
+                                    @else
+                                        <td>{{ $loop->iteration }}</td>
+                                    @endif
+
                                     @if($item['key'] == 'tunggakan' && json_decode($tagihan->column, true)[$key+1]['key'] == 'total_tunggakan')
-                                        <td>{{ $item['label'] }}</td>
+                                        <td class="text-nowrap">{{ $item['label'] }}</td>
                                         <td>{{ $item['value'] }}</td>
 
-                                        <td>{{ formatRp(json_decode($tagihan->column, true)[$key+1]['value']) }}</td>
+                                        <td class="text-nowrap">{{ formatRp(json_decode($tagihan->column, true)[$key+1]['value']) }}</td>
                                     @else
-                                        <td colspan="2">{{ $item['label'] }}</td>
-                                        <td>{{ formatRp($item['value']) }}</td>
+                                        @if($loop->last)
+                                            <td colspan="2" class="text-nowrap fw-bold text-center">{{ $item['label'] }}</td>
+                                            <td class="text-nowrap fw-bold">{{ formatRp($item['value']) }}</td>
+                                        @else
+                                            <td colspan="2" class="text-nowrap">{{ $item['label'] }}</td>
+                                            <td class="text-nowrap">{{ formatRp($item['value']) }}</td>
+                                        @endif
                                     @endif
                                 </tr>
                             @empty
@@ -74,7 +94,31 @@
                         </tbody>
                     </table>
 
-                    <a href="{{ route('tagihan.pdf', $tagihan->id) }}" target="_blank" class="btn btn-primary"><i class="fas fa-file-pdf me-1"></i> Cetak PDF</a>
+                    <a href="{{ route('tagihan.pdf', $tagihan->id) }}" target="_blank" class="btn btn-primary"><i class="fas fa-file-pdf me-1"></i> Download Kartu Kendali</a>
+
+                    <h3 class="mt-3">
+                        Keterangan
+                    </h3>
+                    {!! $tagihan->jenjang->catatan !!}
+                    <div id="ttd">
+                        <table width="100%">
+                            <tr>
+                                <td width="70%"></td>
+                                <td width="30%" class="text-center">
+                                    <div>
+                                        Karanganyar, {{ date('d F Y') }} <Br>
+                                        Bendahara Sekolah,
+                                    </div>
+                                    <div class="my-2">
+                                        <img src="{{ asset('assets/img/ttd.png') }}" alt="ttd" width="100" height="100">
+                                    </div>
+                                    <div id="camat">
+                                        <strong><u>Najmudin, S.Kom</u></strong>
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
             @elseif($nis != '')
                 <div class="col-12 text-center mt-3">
